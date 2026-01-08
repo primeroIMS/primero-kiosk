d3a25c2c5daf07e2e33bb7adaba584e8af5cc653
Joshua Toliver <jtoliver@quoininc.com>
2026-01-08 14:00:52 -0500

    Adding ruby-vite

---
+++ app/frontend/App.tsx
@@ -0,0 +1,7 @@
+import './app.css'
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">This is how fast refresh works</h1>;
+};
+
+export default App;
---
+++ app/frontend/entrypoints/application.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "../App";
+
+const rootElement = document.getElementById("app")!;
+
+if (!rootElement.innerHTML) {
+    const root = createRoot(rootElement);
+    root.render(<App />);
+}
