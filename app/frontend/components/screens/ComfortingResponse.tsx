76f0f69b6ec9ef1ff5ffa714d33a83fd833e18b6
Dennis Hernandez <dhernand@quoininc.com>
2026-02-04 09:10:25 -0600

    Fix styles

--- app/frontend/components/screens/ComfortingResponse.tsx
+++ app/frontend/components/screens/ComfortingResponse.tsx
@@ -10,23 +10,25 @@ type Props = {
 function ComfortingResponse({ config }: Props) {
   return (
     <>
-      {config.featured_image && (
-        <img
-          alt="Featured Image"
-          className="mb-8"
-          src={config.featured_image}
-        />
-      )}
-      {config.title.text?.[i18n.locale] && (
-        <PageTitle color={config.title.color}>
-          {config.title.text[i18n.locale]}
-        </PageTitle>
-      )}
-      {config.description?.text?.[i18n.locale] && (
-        <p className="text-foreground text-center text-lg whitespace-pre-line" style={{ color: config?.description?.color }}>
-          {config.description.text[i18n.locale]}
-        </p>
-      )}
+      <div className="mb-20">
+        {config.featured_image && (
+          <img
+            alt="Featured Image"
+            className="mb-8"
+            src={config.featured_image}
+          />
+        )}
+        {config.title.text?.[i18n.locale] && (
+          <PageTitle color={config.title.color}>
+            {config.title.text[i18n.locale]}
+          </PageTitle>
+        )}
+        {config.description?.text?.[i18n.locale] && (
+          <p className="text-foreground text-center text-lg whitespace-pre-line" style={{ color: config?.description?.color }}>
+            {config.description.text[i18n.locale]}
+          </p>
+        )}
+      </div>
       <div className="flex justify-between">
         <Button
           text="buttons.back"
