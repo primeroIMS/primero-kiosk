05311b6959972df630e13c5dfe4b005f50112dd7
Joshua Toliver <jtoliver@quoininc.com>
2026-02-09 13:46:29 -0500

    Adding parenthesis around method calls

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -10,9 +10,9 @@ class Screen < ApplicationRecord
 
   def self.create_or_update(screen_hash)
     screen = Screen.find_by('data ->> \'id\' = ?', screen_hash[:id])
-    return Screen.create! data: screen_hash if screen.nil?
+    return Screen.create!(data: screen_hash) if screen.nil?
 
-    screen.update! data: screen_hash
+    screen.update!(data: screen_hash)
     screen
   end
 end
