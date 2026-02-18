3c80f8347d3e1cb66c708d13afc9d7bf8eae7f8a
Dennis Hernandez <dhernand@quoininc.com>
2026-02-18 08:58:36 -0600

    Add test

--- spec/services/api_connector/primero_connector_spec.rb
+++ spec/services/api_connector/primero_connector_spec.rb
@@ -13,17 +13,24 @@ describe ApiConnector::PrimeroConnector do
   let(:record_with_id) { record.merge('data' => record['data'].merge('id' => 'case-id')) }
 
   describe '.create' do
-    before do
+    it 'returns the created case' do
       expect(connection).to(
         receive(:post).with('/api/v2/cases', record).and_return([200, record_with_id])
       )
-    end
-
-    it 'returns the created case' do
       result = primero_connector.create({'record_type' => 'case'}.merge(record))
 
       expect(result[:status]).to eq(200)
       expect(result[:response]).to eq(record_with_id)
+    end
+
+    it 'throws an exception for a non-successful status code' do
+      expect(connection).to(
+        receive(:post).with('/api/v2/cases', record).and_return([301, 'Moved permanently'])
+      )
+
+      expect do
+        primero_connector.create({'record_type' => 'case'}.merge(record))
+      end.to raise_error(ApiConnector::UnsuccessfulResponseError)
     end
   end
 end
