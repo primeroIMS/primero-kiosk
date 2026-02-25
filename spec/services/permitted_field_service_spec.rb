6514c8de146b830ab81c2994db2f1ec06bcf36ff
Joshua Toliver <jtoliver@quoininc.com>
2026-02-25 14:56:18 -0500

    Fixing broken rspec

--- spec/services/permitted_field_service_spec.rb
+++ spec/services/permitted_field_service_spec.rb
@@ -3,16 +3,33 @@
 require 'rails_helper'
 
 describe PermittedFieldService do
+  before do
+    @app_flow = AppFlow.create!(
+      data: {
+        id: 'flow1',
+        name: 'Flow 1',
+        description: 'Flow 1 description',
+        start_screen_id: 'screen1'
+      }
+    )
+  end
+
+  after do
+    AppFlow.destroy_all
+    Screen.destroy_all
+  end
+
   let!(:screens) do
     [
       Screen.create!(
+        app_flow: @app_flow,
         data: {
           id: 'screen1',
           bg_color: '#1D1DC2',
           component: 'MultiSelect',
           fields: [
-            { field_id: 'input_1', scope: 'record', backend_id: 'field1' },
-            { field_id: 'input_2', scope: 'global', backend_id: 'field2' }
+            { slot: 'input_1', scope: 'record', backend_id: 'field1' },
+            { slot: 'input_2', scope: 'global', backend_id: 'field2' }
           ],
           title: { text_i18n: { en: 'Screen 1' } },
           flow: {
@@ -24,12 +41,13 @@ describe PermittedFieldService do
         }
       ),
       Screen.create!(
+        app_flow: @app_flow,
         data: {
           id: 'screen2',
           bg_color: '#1D1DC2',
           component: 'MultiSelect',
           fields: [
-            { field_id: 'input_1', scope: 'record', backend_id: 'field3' },
+            { slot: 'input_1', scope: 'record', backend_id: 'field3' }
           ],
           title: { text_i18n: { en: 'Screen 1' } },
           flow: {
