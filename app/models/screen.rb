671347c304398608b27e95847a95284df6749aaf
Joshua Toliver <jtoliver@quoininc.com>
2026-03-06 13:39:54 -0500

    r2-3699: Finding screens by app flow id and screen id for configuration

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -12,7 +12,7 @@ class Screen < ApplicationRecord
 
   def self.create_or_update(app_flow_id:, **screen_hash)
     app_flow = AppFlow.find_by('data ->> \'unique_id\' = ?', app_flow_id)
-    screen = Screen.find_by('data ->> \'id\' = ?', screen_hash[:id])
+    screen = Screen.where(app_flow_id: app_flow.id).find_by('data ->> \'id\' = ?', screen_hash[:id])
 
     return Screen.create!(data: screen_hash, app_flow_id: app_flow.id) if screen.nil?
 
