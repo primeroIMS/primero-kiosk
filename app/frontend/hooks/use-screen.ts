1d0895f710784a75c8fc2fb1c67389f0a21b655c
Joshua Toliver <jtoliver@quoininc.com>
2026-03-23 12:15:59 -0400

    Removing console.logs

--- app/frontend/hooks/use-screen.ts
+++ app/frontend/hooks/use-screen.ts
@@ -101,11 +101,10 @@ function useScreen({
 
     useEffect(() => {
         if (!config.screen.flow.end_of_flow) return;
-        console.log("End of flow reached, attempting to submit to remote...");
+
         const isCaptchaConfigured =
             Boolean(captchaConfig?.provider) && Boolean(captchaConfig?.site_key);
 
-        console.log("Submitting to remote...", isCaptchaConfigured, captchaResponse, captchaConfig);
         if (isCaptchaConfigured && !captchaResponse) {
             console.warn(
                 "Captcha response is required but not available, cannot submit to remote",
